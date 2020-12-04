e34cc54cd1279095a7c7f689281215c6997fef3f
Zaio Klepoyshkov <lisi4ok@gmail.com>
2020-12-04 12:30:11 +0200

    Small refacturing plus adding SimpleCov

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -3,25 +3,25 @@ require 'rails_helper'
 RSpec.describe Item, type: :model do
 
   describe 'validation test' do
-      it 'ensures link presence' do
-        feed = Feed.new(title: 'Test Title', description: 'Test Description').save
-        expect(feed).to eq(false)
-      end
+    it 'ensures link presence' do
+      feed = Feed.new(title: 'Test Title', description: 'Test Description').save
+      expect(feed).to eq(false)
+    end
 
-      it 'ensures title presence' do
-        feed = Feed.new(link: 'Test Link', description: 'Test Description').save
-        expect(feed).to eq(false)
-      end
+    it 'ensures title presence' do
+      feed = Feed.new(link: 'Test Link', description: 'Test Description').save
+      expect(feed).to eq(false)
+    end
 
-      it 'ensures description presence' do
-        feed = Feed.new(link: 'Test Link', title: 'Test Title').save
-        expect(feed).to eq(false)
-      end
+    it 'ensures description presence' do
+      feed = Feed.new(link: 'Test Link', title: 'Test Title').save
+      expect(feed).to eq(false)
+    end
 
-      it 'ensures created' do
-        feed = Feed.new(link: 'Test Link', title: 'Test Title', description: 'Test Description').save
-        expect(feed).to eq(true)
-      end
+    it 'ensures created' do
+      feed = Feed.new(link: 'Test Link', title: 'Test Title', description: 'Test Description').save
+      expect(feed).to eq(true)
+    end
   end
 
 end
