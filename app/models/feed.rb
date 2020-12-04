3a19b12e71156deca265f933cfef6e8051fe8c1e
Zaio Klepoyshkov <lisi4ok@gmail.com>
2020-12-04 10:58:47 +0200

    My first test with rspect - TESTING FEEDS MODEL

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -1,3 +1,7 @@
 class Feed < ApplicationRecord
   has_many :items, dependent: :destroy
+
+  validates :link, presence: true
+  validates :title, presence: true
+  validates :description, presence: true
 end
