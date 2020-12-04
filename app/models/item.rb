4d49996a1d59aecc90412c94304a4b8d2abfd3c4
Zaio Klepoyshkov <lisi4ok@gmail.com>
2020-12-04 11:23:51 +0200

    Items model test

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,8 @@
 class Item < ApplicationRecord
   belongs_to :feed
+
+  validates :link, presence: true
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :feed_id, presence: true
 end
