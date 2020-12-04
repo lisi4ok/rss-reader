b445d1d56a31856d826c67fdd2247495322bbdbd
Zaio Klepoyshkov <lisi4ok@gmail.com>
2020-12-04 15:38:34 +0200

    Clear my code

--- spec/requests/feeds_request_spec.rb
+++ spec/requests/feeds_request_spec.rb
@@ -32,34 +32,4 @@ RSpec.describe "Feeds", type: :request do
     end
   end
 
-  # describe 'GET feeds#edit' do
-  #   let!(:feed) { Feed.create(valid_attributes) }
-  #   it 'returns a success response' do
-  #     get '/feeds/{}/edit', params: { id: feed }
-  #     expect(response).to be_successful
-  #   end
-  # end
-
-  describe 'POST feeds#create' do
-    it 'create feed with valid attributes' do
-      post "/feeds", :params => { :feed => {:title => "Test Title", link: "Test Link", description: "Test Description"} }
-      expect(response).to have_http_status(302)
-      # for 4.0 fake examples I'm sad from rspec 
-      # https://relishapp.com/rspec/rspec-rails/docs/matchers/redirect-to-matcher
-      # https://relishapp.com/rspec/rspec-rails/docs/request-specs/request-spec
-    end
-  end
-
-  # describe 'PUT feeds#update' do
-  #   let!(:feed) { Feed.create(valid_attributes) }
-  #   it 'update feed with valid attributes' do
-  #     before { put :update, id: feed, feed: feed }
-  #     put "/feeds/#{assigns(:feed).id}", , :params => { :feed => { :title => "Test Title", link: "Test Link", description: "Test Description"} }
-  #     expect(response).to have_http_status(302)
-  #     # for 4.0 fake examples I'm sad from rspec 
-  #     # https://relishapp.com/rspec/rspec-rails/docs/matchers/redirect-to-matcher
-  #     # https://relishapp.com/rspec/rspec-rails/docs/request-specs/request-spec
-  #   end
-  # end
-
 end
